b6b1bcaf8b300ee218d5cd450b58b79897bfc774
Vincenzo Pellegrini <dev@enzopellegrini.me>
2022-12-01 09:29:35 +0100

    Fixed description

--- 353607/tm.c
+++ 353607/tm.c
@@ -4,7 +4,6 @@
  *
  * @section LICENSE
  *
- * [...]
  * Copyright © 2022 Vincenzo Pellegrini dev@enzopellegrini.me
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
